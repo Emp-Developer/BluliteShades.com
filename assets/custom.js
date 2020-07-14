e7ecaac2d8a5dc7935e336775d213205008e8b71
Li Mingzhu <61705596+Emp-Developer@users.noreply.github.com>
2020-07-14 14:04:55 +0800

    First Upload

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+(function( $ ) {
+   
+    $(window).scroll(function(){
+      var sticky = $("#header-content"),
+          scroll = $(window).scrollTop();
+  
+      if (scroll >= 46) sticky.addClass('fixed-header');
+      else sticky.removeClass('fixed-header');
+    });
+  
+  })( jQuery );
